69b8c76644519f46f7bcbc2e5978fac761ba03d2
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2024-07-25 21:58:27 +0500

    Create mainScript.js

---
+++ assets/js/mainScript.js
@@ -0,0 +1,13 @@
+const navigation = document.querySelector("div.navigation");
+
+setInterval(() => {
+  if (window.pageYOffset < 225) {
+    if (!navigation.classList.contains("start")) {
+      navigation.classList.add("start");
+    }
+  } else {
+    if (navigation.classList.contains("start")) {
+      navigation.classList.remove("start");
+    }
+  }
+}, 10);
