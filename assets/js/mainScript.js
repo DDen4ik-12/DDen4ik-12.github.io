d6f2b071aa9929cdd4610f7377c880bec5064350
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2024-07-27 19:00:16 +0500

    Update mainScript.js

--- assets/js/mainScript.js
+++ assets/js/mainScript.js
@@ -1,33 +1,3 @@
-const trueSin = (deg) => {
-  if (
-    deg % 360 == 0 ||
-    deg % 360 == 180
-  ) {
-    return 0;
-  } else if (
-    deg % 360 == 90 ||
-    deg % 360 == 270
-  ) {
-    return 1;
-  } else {
-    return Math.sin(deg * Math.PI / 180);
-  }
-};
-const trueCos = (deg) => {
-  if (
-    deg % 360 == 0 ||
-    deg % 360 == 180
-  ) {
-    return 1;
-  } else if (
-    deg % 360 == 90 ||
-    deg % 360 == 270
-  ) {
-    return 0;
-  } else {
-    return Math.cos(deg * Math.PI / 180);
-  }
-};
 const navigation = document.querySelector("div.navigation");
 const navigationLogo = document.querySelector("a.navigation_logo");
 
