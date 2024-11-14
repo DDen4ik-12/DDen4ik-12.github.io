a4fddc3ca5bd81210a7b595483f1c51e6f3ce8ab
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2024-11-14 14:53:51 +0500

    Update mainScript.js

--- assets/js/mainScript.js
+++ assets/js/mainScript.js
@@ -12,16 +12,3 @@ setInterval(() => {
     }
   }
 }, 10);
-
-const downloadUrl = (url, file) => {
-  const link = document.createElement("a");
-  link.href = url;
-  link.download = file;
-  document.body.appendChild(link);
-  link.click();
-  link.remove();
-};
-switch (new URLSearchParams(location.search).get("giftcode")) {
-  case "gift":
-    downloadUrl("/files/пин конца (1).sprite3", "пин.sprite3");
-}
