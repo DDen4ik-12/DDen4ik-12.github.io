59dac0d4f3c0db94dff0031db3f86f9400112bf1
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2025-04-06 13:41:35 +0500

    Update mainScript.js

--- assets/js/mainScript.js
+++ assets/js/mainScript.js
@@ -1,5 +1,28 @@
 const navigation = document.querySelector("div.navigation");
+const navigationPictureBackground = document.querySelector("div.navigation > div > div");
 const metaThemeColor = document.querySelector('meta[name="theme-color"]');
+
+const hsl2Rgb = (h, s, l) => {
+  let r, g, b;
+  if (s === 0) {
+    r = g = b = l;
+  } else {
+    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    const p = 2 * l - q;
+    r = hue2Rgb(p, q, h + 1/3);
+    g = hue2Rgb(p, q, h);
+    b = hue2Rgb(p, q, h - 1/3);
+  }
+  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+const hue2Rgb = (p, q, t) => {
+  if (t < 0) t += 1;
+  if (t > 1) t -= 1;
+  if (t < 1/6) return p + (q - p) * 6 * t;
+  if (t < 1/2) return q;
+  if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+  return p;
+}
 
 if (typeof ((new URL(location.href)).searchParams.get("randCol") ?? undefined) != "undefined") {
   const randomColorMode = new Object();
@@ -24,6 +47,17 @@ if (typeof ((new URL(location.href)).searchParams.get("randCol") ?? undefined) !
     }`;
   metaThemeColor.content = `hsl(${randomColorMode.looksMain.hue}deg ${randomColorMode.looksMain.saturation}% 50%)`;
   document.head.appendChild(randomColorMode.style);
+  if (location.pathname == "/projects/") {
+    Object.assign(
+      randomColorMode.looksMain,
+      ((x) => {
+        return {red: x[0], green: x[1], blue: x[2]};
+      })(hsl2Rgb(randomColorMode.looksMain.hue, randomColorMode.looksMain.saturation / 100, 0.5))
+    )
+    navigationPictureBackground.style.filter = `url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg'><filter id='filter' color-interpolation-filters='sRGB'><feColorMatrix type='matrix' values='0.667 0.667 0.666 0 ${
+      (randomColorMode.looksMain.red / 255) - 1
+    } 0.667 0.667 0.666 0 ${(randomColorMode.looksMain.green / 255) - 1} 0.667 0.667 0.666 0 ${(randomColorMode.looksMain.blue / 255) - 1} 0 0 0 1 0'></feColorMatrix></filter></svg>#filter")`
+  }
 
   document.querySelectorAll("a").forEach((x) => {
     const href = new URL(x.href);
