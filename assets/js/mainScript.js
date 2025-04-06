f37b72abc6d26882bc28a6539a460f05999a6d63
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2025-04-06 13:47:51 +0500

    Update mainScript.js

--- assets/js/mainScript.js
+++ assets/js/mainScript.js
@@ -9,18 +9,18 @@ const hsl2Rgb = (h, s, l) => {
   } else {
     const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
     const p = 2 * l - q;
-    r = hue2Rgb(p, q, h + 1/3);
+    r = hue2Rgb(p, q, h + 1 / 3);
     g = hue2Rgb(p, q, h);
-    b = hue2Rgb(p, q, h - 1/3);
+    b = hue2Rgb(p, q, h - 1 / 3);
   }
   return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
 }
 const hue2Rgb = (p, q, t) => {
   if (t < 0) t += 1;
   if (t > 1) t -= 1;
-  if (t < 1/6) return p + (q - p) * 6 * t;
-  if (t < 1/2) return q;
-  if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+  if (t < 1 / 6) return p + (q - p) * 6 * t;
+  if (t < 1 / 2) return q;
+  if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
   return p;
 }
 
@@ -52,7 +52,7 @@ if (typeof ((new URL(location.href)).searchParams.get("randCol") ?? undefined) !
       randomColorMode.looksMain,
       ((x) => {
         return {red: x[0], green: x[1], blue: x[2]};
-      })(hsl2Rgb(randomColorMode.looksMain.hue, randomColorMode.looksMain.saturation / 100, 0.5))
+      })(hsl2Rgb(randomColorMode.looksMain.hue / 360, randomColorMode.looksMain.saturation / 100, 0.5))
     )
     navigationPictureBackground.style.filter = `url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg'><filter id='filter' color-interpolation-filters='sRGB'><feColorMatrix type='matrix' values='0.667 0.667 0.666 0 ${
       (randomColorMode.looksMain.red / 255) - 1
